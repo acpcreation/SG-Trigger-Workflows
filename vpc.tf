23cd8e58cc6f6083abe51914c250940b6e2030dc
Alex Plattel <acpcreation@Alexs-MacBook-Pro.local>
2025-12-16 14:25:03 -0500

    fix data

--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,19 @@
 # Virtual Private Cloud (VPC) - isolated network environment in AWS
 # Commented out to use existing VPC instead of creating a new one
-# resource "aws_vpc" "vpc_cidr"{
-#     cidr_block = "10.0.0.0/16"           # IP address range for the VPC (65,536 IP addresses)
-#     enable_dns_hostnames = true          # Enable DNS hostnames for instances
-#     enable_dns_support = true            # Enable DNS resolution
-#
-#     tags = {
-#         Name = "web-vpc"
-#     }
-# }
+resource "aws_vpc" "vpc_cidr"{
+    cidr_block = "10.0.0.0/16"           # IP address range for the VPC (65,536 IP addresses)
+    enable_dns_hostnames = true          # Enable DNS hostnames for instances
+    enable_dns_support = true            # Enable DNS resolution
+
+    tags = {
+        Name = "web-vpc"
+    }
+}
 
 # Data source to reference existing VPC
-data "aws_vpc" "vpc_cidr" {
-    id = "vpc-064c66aaba2ac7fed"  # Using specified VPC ID
-}
+# data "aws_vpc" "vpc_cidr" {
+#     id = "vpc-064c66aaba2ac7fed"  # Using specified VPC ID
+# }
 
 # Public subnet where the EC2 instance will be placed
 resource "aws_subnet" "web_subnet" {
@@ -29,21 +29,21 @@ resource "aws_subnet" "web_subnet" {
 
 # Internet Gateway - allows internet access to/from the VPC
 # Commented out to use existing internet gateway
-# resource "aws_internet_gateway" "web_igw" {
-#     vpc_id = data.aws_vpc.vpc_cidr.id
-#
-#     tags = {
-#         Name = "web-igw"
-#     }
-# }
+resource "aws_internet_gateway" "web_igw" {
+    vpc_id = data.aws_vpc.vpc_cidr.id
 
-# Data source to reference existing internet gateway
-data "aws_internet_gateway" "web_igw" {
-    filter {
-        name   = "attachment.vpc-id"
-        values = [data.aws_vpc.vpc_cidr.id]
+    tags = {
+        Name = "web-igw"
     }
 }
+
+# Data source to reference existing internet gateway
+# data "aws_internet_gateway" "web_igw" {
+#     filter {
+#         name   = "attachment.vpc-id"
+#         values = [data.aws_vpc.vpc_cidr.id]
+#     }
+# }
 
 # Route table - defines network routing rules
 resource "aws_route_table" "web_rt" {
